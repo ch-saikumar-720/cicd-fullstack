f634de869ce9d78465010b56632327db5ff9b47d
ch-saikumar-720 <158131442+ch-saikumar-720@users.noreply.github.com>
2025-04-30 20:55:15 +0530

    Update Signup.js

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -14,7 +14,7 @@ function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://.*:5000/signup", formData);
+      const response = await axios.post("http://.*/signup", formData);
       alert(response.data.message);
       navigate("/login");
     } catch (error) {
